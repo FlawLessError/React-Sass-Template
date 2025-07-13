69d70b770a709f24c1f714e9992cea314d90f33b
FlawLessError <50498066+FlawLessError@users.noreply.github.com>
2025-07-13 22:08:37 +0100

    Minor change
    Some comments for sass and added PWA using vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicons/favicon.ico", "favicons/apple-touch-icon.png"],
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "AppName",
+        short_name: "Short AppName",
+        description: "My Awesome App description",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
